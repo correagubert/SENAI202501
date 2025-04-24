fb05ae1c2387209d400f0b04480923001f1a7121
NICOLAS CORRÊA GUBERT <nicolas_gubert@estudante.sesisenai.org.br>
2025-04-23 22:16:52 -0300

    Iniciado projeto da 'Farmárcia'.

---
+++ projeto-farmacia/src/components/ContainerPromo.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './ContainerPromo.css'
+
+function ContainerPromo() {
+  return (
+    <div className='container-promo'>
+        <img src="img/farmarcia-camiseta.png" alt="Camiseta da Farmárcia - Grande" />
+        <div className='promocao'>
+            <h2>Quer nos apoiar?</h2>
+            <p>Durante tempo LIMITADO, a Farmácia oferece camisetas promocionais suuuuuper-bacanudas para os clientes que realmente acreditam no projeto, e que gostariam de por um pouco de investimento nesse incrível futuro da Farmárcia. Temos todos os tamanhos, em malha de algodão.</p>
+        </div>
+      </div>
+  )
+}
+
+export default ContainerPromo
